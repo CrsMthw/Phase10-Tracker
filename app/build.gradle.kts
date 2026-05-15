cb458429e4ed797028aeffcb0b99f50153403448
crsmthw <crsmthw@crsmthw.com>
2026-05-15 15:25:03 +0300

    fixed version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.crsmthw.phase10tracker"
         minSdk = 35
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
